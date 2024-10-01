04f12a84e20c21d277e58b60b26ed5c1ce339dd2
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2024-10-01 23:22:02 +0600

    added login api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 const { User } = require("../models/user.model");
 const { getErrorResponse, getSuccessResponse } = require("../utils/responseHandlers");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
     createUser: async (request, response) => {
@@ -12,8 +14,8 @@ module.exports = {
                 const user = new User();
                 const existingUser = await user.findUserByEmail(email);
 
-                if (existingUser && existingUser.length === 0) {
-                    const newUser = await user.createUser({
+                if (!existingUser) {
+                    await user.createUser({
                         username,
                         email,
                         password,
@@ -24,8 +26,66 @@ module.exports = {
                         message: "Registration is successful, Please login"
                     });
                 }
-                else if (existingUser && existingUser.length > 0) {
+                else {
                     return getErrorResponse({ response, code: 409, message: "Email already exists" });
+                }
+            }
+        } catch (err) {
+            console.log("error", err.message);
+
+            return getErrorResponse({ response, code: 500, message: "Something went wrong!" });
+        }
+    },
+    login: async (request, response) => {
+        try {
+            const { email, password } = request.body;
+
+            if (!(email && password)) {
+                return getErrorResponse({ response, code: 400, message: "Missing required fields" });
+            } else {
+                const user = new User();
+                const existingUser = await user.findUserByEmail(email);
+
+                if (existingUser?.email) {
+                    const isPasswordCorrect = await bcrypt.compare(password, existingUser?.password);
+                    if (isPasswordCorrect) {
+                        // password is correct
+                        // generate jwt token
+                        const token = jwt.sign(
+                            {
+                                id: existingUser.id,
+                                email: existingUser.email,
+                                userName: existingUser.userName,
+                            },
+                            process.env.TOKEN_KEY,
+                            {
+                                expiresIn: process.env.TOKEN_EXIPRATION_TIME,
+                            }
+                        );
+                        // create expiraion timestamp
+                        const expirationDuration = Number(process.env.TOKEN_EXIPRATION_TIME.split("s")[0]);
+
+                        const expirationTimestamp = Math.floor(Date.now() / 1000) + expirationDuration;
+                        // store tokena and timestamp in db
+                        const isTokenAndExpirationTimeSavedInDB = await user.login({ token, expirationTimestamp, userId: existingUser.id })
+
+                        // once token and timestamp is saved send user success response with his information
+                        const userData = { ...existingUser, token };
+                        // remove the password
+                        delete userData.password;
+
+                        return getSuccessResponse({
+                            response,
+                            code: 200,
+                            message: "Login is successful",
+                            data: userData
+                        });
+
+                    }
+                    return getErrorResponse({ response, code: 409, message: "Email already exists" });
+                }
+                else {
+                    return getErrorResponse({ response, code: 401, message: "Invalid login or password. Please try again." });
                 }
             }
         } catch (err) {
--- models/user.model.js
+++ models/user.model.js
@@ -7,13 +7,18 @@ class User{
     }
 
     async findUserByEmail(email){
-        const [results] = await db.execute(`SELECT * FROM users where email=?`,[email]);
-        return results;
+        const [results] = await db.execute(`SELECT * FROM users where email=?`,[email]);        
+        return results[0];
     }
 
     async createUser({username, password, email}){
         const [results] = await db.execute(`INSERT INTO users(username, email, password) values(?,?,?)`,[username, email, await bcrypt.hash(password, 10)]);
-        return results;
+        return results[0];
+    }
+
+    async login({token, expirationTimestamp, userId}){
+        const [results] = await db.execute(`UPDATE users SET token=?, token_expiration_time=? where id=?`,[token, expirationTimestamp, userId]);
+        return results[0];
     }
 }
 
